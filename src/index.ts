c7119158ea3561b671385e6d90548b37a62a919a
John Simoni <john.simoni@hiya.com>
2023-09-14 15:30:44 -0400

    Leverage camera-access to view AR image

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
 
 import * as Babylon from '@babylonjs/core'
 
-async function createBabylonScene(engine: Babylon.Engine, canvas: HTMLCanvasElement): Promise<Babylon.Scene> {
+async function createBabylonScene(engine: Babylon.Engine, canvas: HTMLCanvasElement) {
     const scene = new Babylon.Scene(engine)
 
     const camera = new Babylon.FreeCamera(
@@ -15,39 +15,60 @@ async function createBabylonScene(engine: Babylon.Engine, canvas: HTMLCanvasElem
     )
     camera.setTarget(Babylon.Vector3.Zero())
     camera.attachControl(canvas, true)
-  
+
     const light = new Babylon.HemisphericLight("light1", new Babylon.Vector3(0, 1, 0), scene)
     light.intensity = 0.7
   
     const xr = await scene.createDefaultXRExperienceAsync({
       uiOptions: {
         sessionMode: "immersive-ar",
+        requiredFeatures: [ 
+            // Babylon.WebXRFeatureName.ANCHOR_SYSTEM, 
+            // Babylon.WebXRFeatureName.HIT_TEST, 
+            "dom-overlay",
+            "camera-access",
+        ]
       },
     })
 
-    const anchorSystem = xr.baseExperience.featuresManager.enableFeature(
-        Babylon.WebXRAnchorSystem,
-    ) as Babylon.WebXRAnchorSystem
-    const hitTest = xr.baseExperience.featuresManager.enableFeature(
-        Babylon.WebXRHitTest, 
-        "latest",
-        { enableTransientHitTest: true, disablePermanentHitTest: true },
-    ) as Babylon.WebXRHitTest
-    // const domOverlay = xr.baseExperience.featuresManager.enableFeature(
-    //     Babylon.WebXRDomOverlay,
+    // const anchorSystem = xr.baseExperience.featuresManager.enableFeature(
+    //     Babylon.WebXRAnchorSystem,
+    //     undefined,
+    //     undefined,
+    //     undefined,
+    //     true
+    // ) as Babylon.WebXRAnchorSystem
+    // const hitTest = xr.baseExperience.featuresManager.enableFeature(
+    //     Babylon.WebXRHitTest, 
     //     "latest",
-    //     { element: "#dom-overlay-container" }
-    // )
+    //     { enableTransientHitTest: true, disablePermanentHitTest: true },
+    //     undefined,
+    //     true,
+    // ) as Babylon.WebXRHitTest
 
-    return scene
+    xr.baseExperience.featuresManager.enableFeature(
+        Babylon.WebXRDomOverlay,
+        "latest",
+        { element: "#dom-overlay-container" },
+    )
+
+    return { scene, xr }
 }
 
-async function detectHands(handPoseDetector: handPoseDetection.HandDetector, canvas: HTMLCanvasElement, scene: Babylon.Scene) {
-    const [ estimates ] = await handPoseDetector.estimateHands(canvas, {})
+function detectHands(xrSessionManager: Babylon.WebXRSessionManager, handDetector: handPoseDetection.HandDetector, currentFrame: XRFrame) {
+    const textChild = document.querySelector('#dom-overlay-text')
+    if (!textChild) throw new Error("Could not find DOM Overlay")
 
-    console.log(
-        JSON.stringify(estimates.keypoints)
-    )
+    const referenceSpace = xrSessionManager.referenceSpace
+
+    const viewerPose = currentFrame.getViewerPose(referenceSpace)
+    for (const view of viewerPose?.views ?? []) {
+        if ((view as { camera?: any } & XRView).camera) {
+            textChild.textContent = 'Found view with camera'
+        }
+    }
+
+    xrSessionManager.session.requestAnimationFrame((time, xrFrame) => detectHands(xrSessionManager, handDetector, xrFrame))
 }
 
 async function main() {
@@ -55,16 +76,30 @@ async function main() {
     if (!canvas) throw new Error("Canvas elemet doesn't exist")
 
     const babylonEngine = await Babylon.EngineFactory.CreateAsync(canvas, {})
-    const babylonScene = await createBabylonScene(babylonEngine, canvas)
+    const {
+        scene: babylonScene,
+        xr: babylonXRBase
+    } = await createBabylonScene(babylonEngine, canvas)
+
+    babylonEngine.runRenderLoop(babylonScene.render)
+    window.addEventListener('resize', () => babylonEngine.resize())
 
     const handDetector = await handPoseDetection.createDetector(
         handPoseDetection.SupportedModels.MediaPipeHands,
         { runtime: 'tfjs' }
     )
 
-    babylonEngine.runRenderLoop(async () => {
-        await detectHands(handDetector, canvas, babylonScene)
-        return babylonScene.render()
+    const xrSessionManager = babylonXRBase.baseExperience.sessionManager
+    const xrSessionInitObserver = xrSessionManager.onXRSessionInit.addOnce((xrSession) => {
+        xrSession.requestAnimationFrame((timestamp, xrFrame) => {
+            detectHands(xrSessionManager, handDetector, xrFrame)
+        })
     })
-    window.addEventListener('resize', () => babylonEngine.resize())
+
+    xrSessionManager.onXRSessionEnded.remove(xrSessionInitObserver)
 }
+
+main()
+    .catch((e) => {
+        window.alert(e.message)
+    })
